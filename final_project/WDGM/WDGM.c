b48230838fc2193b970651ed05502f8575d57ff7
AliBadran716 <102072821+AliBadran716@users.noreply.github.com>
2024-06-23 18:07:22 +0200

    ,

---
+++ final_project/WDGM/WDGM.c
@@ -0,0 +1,23 @@
+/*
+ * WDGM.c
+ *
+ *  Created on: Jun 23, 2024
+ *      Author: Kimo Store
+ */
+#include "WDGM.h"
+
+void WDGM_Init(void){
+
+}
+
+void WDGM_MainFunction(void){
+
+}
+
+WDGM_StatusType WDGM_PovideSuppervisionStatus(void){
+
+}
+
+void WDGM_AlivenessIndication(void){
+
+}
--- final_project/gpio/gpio.c
+++ final_project/gpio/gpio.c
@@ -12,84 +12,84 @@
 
 void GPIO_Init(uint8 port, uint8 pin,uint8 direction)
 {
-    switch(port)
-    {
-        case PORT_B:
-            if(direction == OUTPUT)
-            {
-                SET_BIT(DDRB,pin);
-                SET_BIT(PORTB,pin);
-            }
-            else
-            {
-                CLEAR_BIT(DDRB,pin);
-                SET_BIT(PORTB,pin);
-            }
-            break;
-        case PORT_C:
-            if(direction == OUTPUT)
-            {
-                SET_BIT(DDRC,pin);
-                SET_BIT(PORTB,pin);
-            }
-            else
-            {
-                CLEAR_BIT(DDRC,pin);
-                SET_BIT(PORTC,pin);
-            }
-            break;
-        case PORT_D:
-            if(direction == OUTPUT)
-            {
-                SET_BIT(DDRD,pin);
-                SET_BIT(PORTB,pin);
-            }
-            else
-            {
-                CLEAR_BIT(DDRD,pin);
-                SET_BIT(PORTD,pin);
-            }
-            break;
-        default:
-            break;
-    }
+	switch(port)
+	{
+	case PORT_B:
+		if(direction == OUTPUT)
+		{
+			SET_BIT(DDRB,pin);
+			SET_BIT(PORTB,pin);
+		}
+		else
+		{
+			CLEAR_BIT(DDRB,pin);
+			SET_BIT(PORTB,pin);
+		}
+		break;
+	case PORT_C:
+		if(direction == OUTPUT)
+		{
+			SET_BIT(DDRC,pin);
+			SET_BIT(PORTB,pin);
+		}
+		else
+		{
+			CLEAR_BIT(DDRC,pin);
+			SET_BIT(PORTC,pin);
+		}
+		break;
+	case PORT_D:
+		if(direction == OUTPUT)
+		{
+			SET_BIT(DDRD,pin);
+			SET_BIT(PORTB,pin);
+		}
+		else
+		{
+			CLEAR_BIT(DDRD,pin);
+			SET_BIT(PORTD,pin);
+		}
+		break;
+	default:
+		break;
+	}
 }
 
 void GPIO_Write(uint8 port, uint8 pin, uint8 value)
 {
-    switch(port)
-    {
-        case PORT_B:
-            if(value == HIGH)
-            {
-                SET_BIT(PORTB,pin);
-            }
-            else
-            {
-                CLEAR_BIT(PORTB,pin);
-            }
-            break;
-        case PORT_C:
-            if(value == HIGH)
-            {
-                SET_BIT(PORTC,pin);
-            }
-            else
-            {
-                CLEAR_BIT(PORTC,pin);
-            }
-            break;
-        case PORT_D:
-            if(value == HIGH)
-            {
-                SET_BIT(PORTD,pin);
-            }
-            else
-            {
-                CLEAR_BIT(PORTD,pin);
-            }
-            break;
-        default:
-            break;
-    }
+	switch(port)
+	{
+	case PORT_B:
+		if(value == HIGH)
+		{
+			SET_BIT(PORTB,pin);
+		}
+		else
+		{
+			CLEAR_BIT(PORTB,pin);
+		}
+		break;
+	case PORT_C:
+		if(value == HIGH)
+		{
+			SET_BIT(PORTC,pin);
+		}
+		else
+		{
+			CLEAR_BIT(PORTC,pin);
+		}
+		break;
+	case PORT_D:
+		if(value == HIGH)
+		{
+			SET_BIT(PORTD,pin);
+		}
+		else
+		{
+			CLEAR_BIT(PORTD,pin);
+		}
+		break;
+	default:
+		break;
+	}
 }
