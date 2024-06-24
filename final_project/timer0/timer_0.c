3567ac01be15ba8454b5633285aa3324c7496e98
Abdulrahman Emad <104274128+abduelrahmanemad@users.noreply.github.com>
2024-06-24 03:58:58 +0300

    comments

--- final_project/timer0/timer_0.c
+++ final_project/timer0/timer_0.c
@@ -1,21 +1,19 @@
-/*
- * timer_0.c
- *
- *  Created on: Jun 23, 2024
- *      Author: DELL
- */
+
 
 
 #include "timer_0.h"
 #include "timer_0_private.h"
 #include "../lib/Bit_Operations.h"
 
+
+/*
+    * Description: This function is responsible for initializing Timer 0
+*/
 void timer_0_init(void)
 {
     TCNT0 = 0;
 
     TCCR0A |= (1 << WGM01) | (1<<COM0A1);
-//    TCCR0B = (1 << FOC0A) | (1 << FOC0B);
     TCCR0B = (1 << CS02) | (1 << CS00); // pre-scaler = 1024
     OCR0A = 155; // Interrupt Every 20ms
 
